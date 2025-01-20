8ba11de379cfe1500100a89d647e70d37a73ae65
Rakshitsurendra <96045800+Rakshitsurendra@users.noreply.github.com>
2025-01-20 19:24:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "spp_terraform_CICD"
+
+    workspaces {
+      name = "spp_dev"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
